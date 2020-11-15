7f18c3fc594722510c3481c73fb98b49573f81ad
Bartek Domanski <bdomanski57@gmail.com>
2020-11-15 14:16:38 +0100

    class imports fixed

--- src/main/java/pl/shoplist/service/ItemService.java
+++ src/main/java/pl/shoplist/service/ItemService.java
@@ -1,20 +1,16 @@
 package pl.shoplist.service;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 import pl.shoplist.common.ItemsNotFoundException;
 import pl.shoplist.model.Item;
 import pl.shoplist.model.ShoppingList;
 import pl.shoplist.repository.ItemRepository;
 import pl.shoplist.repository.ShoppingListRepository;
-
-import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 @Service
 public class ItemService {
